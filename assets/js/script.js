a14483d6bd3c29db5258ae179cea0b97de67f30e
Roland Amoguis <rolandamoguis21@gmail.com>
2025-04-10 20:48:43 +0800

    little changes in html, css

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const usernameInput = document.getElementById("username-input");
+const searchBtn = document.getElementById("search-btn");
+const userProfile = document.getElementById("user-profile");
+const repositoryListContainer = document.getElementById(
+  "repository-list-container"
+);
+
+// TODO: Functionality and API fetch..
