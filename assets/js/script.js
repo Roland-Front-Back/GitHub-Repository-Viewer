3cbf0891ba95a14ae03b436512d2981197ea27e4
Roland Amoguis <rolandamoguis21@gmail.com>
2025-04-11 13:58:35 +0800

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,47 @@
-const usernameInput = document.getElementById("username-input");
+const usernameInput = document
+  .getElementById("username-input")
+  .value.toLowerCase();
 const searchBtn = document.getElementById("search-btn");
-const userProfile = document.getElementById("user-profile");
+const userProfileContainer = document.getElementById("user-profile-container");
 const repositoryListContainer = document.getElementById(
   "repository-list-container"
 );
 
-// TODO: Functionality and API fetch..
+const username = "roland-front-back";
+
+// GitHub API's
+const gitHubUsers = `https://api.github.com/users/${username}/`;
+const gitHubRepos = `https://api.github.com/users/${username}/repos`;
+
+const fetchUser = async () => {
+  try {
+    const res = await fetch(gitHubRepos);
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const fetchRepos = async () => {
+  try {
+    const res = await fetch(gitHubRepos);
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fetchUser();
+fetchRepos();
+
+const showProfileResults = (data) => {
+  const { owner } = data;
+
+  userProfileContainer.innerHTML = `
+    <div class="user">
+    <a href="${url}">Roland</a>
+    </div>
+    `;
+};
